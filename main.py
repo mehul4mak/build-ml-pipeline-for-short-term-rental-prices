1b3169eddc6cf4d8b2301ce17004d5599a2cbf74
mehul4mak <mehul4mak@gmail.com>
2024-06-19 01:44:19 +0530

    regression code

--- main.py
+++ main.py
@@ -128,11 +128,16 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
 
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                version=config['main']['branch_name'],
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                    },
+                env_manager="local"
+                )
 
 
 if __name__ == "__main__":
